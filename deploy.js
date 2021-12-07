d0869d8cc1101628b4c8af71af8555a3c95b2881
cezainmajeed <cezainmajeed0713@gmail.com>
2021-12-07 10:39:22 +0530

    deployed contracts in remix

--- deploy.js
+++ deploy.js
@@ -1,1 +1,29 @@
-// deploy code will go here
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+
+const {interface,bytecode} = require("./compile");
+
+const provider = new HDWalletProvider(
+  process.env.PNEUMONIC,
+  process.env.TEST_NETWORK
+);
+const web3 = new Web3(provider);
+
+
+const deploy = async()=> {
+  const accounts=await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account",accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data:bytecode,
+      arguments:["Hello world!"]
+    })
+    .send({
+      gas:1000000,
+      from:accounts[0]
+    });
+
+    console.log("Contract is deployed to",result.options.address);
+};
+deploy();
